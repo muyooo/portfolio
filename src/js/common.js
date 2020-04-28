ffbdabfdf1c35d7b1799ce6511b2c887cba87315
muyooo <muyooo.info@gmail.com>
2020-04-28 23:45:37 +0900

    Add beerPoint first-mocup

--- src/js/common.js
+++ src/js/common.js
@@ -194,7 +194,7 @@ function worksPreview(index) {
         '2020/03〜開発中'
       ],
       otherTitle: ['リンク'],
-      otherContent: ['<a class="works__feature-link" href="https://note.com/muyooo/n/nc8d15d554bfa?magazine_key=mdfbc6d9e2326" target="_blank" rel="noopener noreferrer">開発日誌</a><br><a class="works__feature-link" href="">最初の試作</a>']
+      otherContent: ['<a class="works__feature-link" href="https://note.com/muyooo/n/nc8d15d554bfa?magazine_key=mdfbc6d9e2326" target="_blank" rel="noopener noreferrer">開発日誌</a><br><a class="works__feature-link" href="dev/beerpoint_first_mocup/index.html">最初の試作</a>']
     }
   ];
   // Set details
